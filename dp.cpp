205cd26f910bce2d0d80e1435fbc2380a343ddf6
Divyansh Joshi <2023ucp1622@mnit.ac.in>
2024-10-10 12:50:02 +0530

    dot product code added

---
+++ dp.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+vector<int> v1 ={1,2,3};
+vector<int> v2 ={3,4,5};
+vector<int> v3;
+for(int i=0;i<3;++i){
+v3.push_back(v1[i]*v2[i]);
+}
+for(int i=0;i<3;++i){
+cout<<v3[i]<<endl;
+}
+return 0;
+}
